1a49c8014c83c0f66745b8a7d9e045d587399ce3
Prakash Gurung <prakash.gurung@hazesoft.co>
2026-07-06 03:47:35 +0000

    Implement LLM integration for Laravel documentation queries

--- query.py
+++ query.py
@@ -1,5 +1,9 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
 
 embeddings = HuggingFaceEmbeddings(
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
@@ -7,9 +11,18 @@ embeddings = HuggingFaceEmbeddings(
 
 chroma = Chroma(persist_directory="./chroma_db", embedding_function = embeddings)
 
-results = chroma.similarity_search("what do i create a controller?", k=3)
+user_prompt = "In laravel 13 , how do i define a single-action controller?"
 
-for result in results:
-    print(result.metadata["source"])
-    print(result.page_content[:200])
-    print("--------------------------------------------------")
+results = chroma.similarity_search_with_score(user_prompt, k=15)
+
+ragContext = ""
+for result, score in results:
+    ragContext += result.metadata["source"] + ": " + result.page_content + "\n"
+
+system_prompt = "You are a documentation assistant for laravel 13. You are given a context and a question. Answer the question based on the context along with the source used for the context. If the answer is not in the context, say 'The answer is not in the context.'."
+final_prompt = f"{system_prompt}\n\nContext:\n{ragContext}\n\nQuestion: {user_prompt}"
+model="gemini-2.5-flash"
+llm = ChatGoogleGenerativeAI(model=model)
+
+result = llm.invoke(final_prompt)
+print(result.content)
